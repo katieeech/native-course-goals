826f441fb691a2d8e92071853fbf190af388b32c
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2022-01-14 18:07:07 +0900

    Finished version of Goal app

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, Button, FlatList } from "react-native";
+
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+  const addGoalHandler = (goalTitle) => {
+    setCourseGoals([
+      ...courseGoals,
+      { id: Math.random().toString(), value: goalTitle },
+    ]);
+    setIsAddMode(false);
+    // setCourseGoals((currentGoals) => [
+    //   ...currentGoals,
+    //   { id: Math.random().toString(), value: goalTitle },
+    // ]);
+  };
+
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals(courseGoals.filter((goal) => goal.id !== goalId));
+  };
+  // const removeGoalHandler = (goalId) => {
+  //   setCourseGoals((currentGoals) => {
+  //     return currentGoals.filter((goal) => goal.id !== goalId);
+  //   });
+  // };
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput
+        isAddMode={isAddMode}
+        addGoalHandler={addGoalHandler}
+        cancelGoalAdditionHandler={cancelGoalAdditionHandler}
+      />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <GoalItem
+            id={itemData.item.id}
+            removeGoalHandler={removeGoalHandler}
+            title={itemData.item.value}
+          />
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
 });
